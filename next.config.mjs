dec07ffc9e539dc16c976e7620950ed3b7d29d6b
Muneer Ibn Fawaz pvt.ltd <avikrnr369@gmail.com>
2026-09-21 11:47:26 +0530

    Add CORS headers for API routes

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,19 @@ const nextConfig = {
       "./node_modules/@sparticuz/chromium/bin/**/*",
       "./node_modules/pdfkit/js/data/**/*"
     ]
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Or replace with specific domain, e.g., "https://staging.mifcortex.com"
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" },
+        ]
+      }
+    ]
   }
 };
 
